f6777fd129c6827e784af7a6606538d55aab256a
East <eastmaels@gmail.com>
2018-03-20 16:41:05 +0800

    do not output new line char

--- web/delete-users.php
+++ web/delete-users.php
@@ -2,11 +2,10 @@
 header("Access-Control-Allow-Origin: *");
 
 exec("echo '' > " . "./users/list");
-// write to device log
 $userList = file("./users/list");
 
 $response = array(
-    'users' => $userList
+    'users' => array_filter($userList)
   );
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($response);
