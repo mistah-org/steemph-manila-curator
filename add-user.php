1a6443fcdd4ac5ed6a782215ffb708259f371444
East <eastmaels@gmail.com>
2018-03-20 11:40:13 +0800

    added add-user.php API

---
+++ add-user.php
@@ -0,0 +1,17 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$user = strtolower(urldecode($_GET["user"]));
+
+// write to device log
+$userList = fopen("./users/list" , "a") or die("Unable to open file!");
+fputcsv($userList, $user);
+fclose($userList);
+
+$response = array(
+    'user' => $user
+  );
+header('Content-type:application/json;charset=utf-8');
+echo json_encode($response);
+
+?>
