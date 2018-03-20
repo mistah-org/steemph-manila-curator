ac47623812776a3f471bc50dfcd75e255050924a
East <eastmaels@gmail.com>
2018-03-20 14:55:24 +0800

    write to users file using implode

--- web/add-user.php
+++ web/add-user.php
@@ -4,13 +4,22 @@ header("Access-Control-Allow-Origin: *");
 $user = strtolower(urldecode($_GET["user"]));
 
 // write to device log
-$userList = file("./users/list");
-$userList = array_unique($userList);
-file_put_contents('./users/list', print_r($userList, true));
+$filename = "./users/list";
+$fp = fopen($filename, 'r');
+$users = array();
+while (!feof($fp))
+  $line=fgets($fp);
+  $line=trim($line);
+  $users[]=$line;
+}
+fclose($fp);
+
+$uniqueUsers = array_unique($users);
+file_put_contents('./users/list', implode(PHP_EOL, $uniqueUsers));
 
 $response = array(
     'user' => $user,
-    'userlist' => file("./users/list")
+    'userlist' => $userArray
   );
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($response);
