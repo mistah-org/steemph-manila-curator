bcc7675730cf593656eeb0864148ff009494ba96
East <eastmaels@gmail.com>
2018-03-20 16:44:22 +0800

    do not output new line char as delete users response

--- web/delete-users.php
+++ web/delete-users.php
@@ -1,11 +1,22 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-exec("echo '' > " . "./users/list");
-$userList = file("./users/list");
+$filename = "./users/list";
+exec("echo '' > " . $filename);
+$fp = fopen($filename, 'r');
+$users = array();
+while (!feof($fp)) {
+  $line=fgets($fp);
+  $line=trim($line);
+  if ($line !== '') {
+    $users[]=$line;
+  }
+}
+$users[]=$user;
+fclose($fp);
 
 $response = array(
-    'users' => array_filter($userList)
+    'users' => array_filter($users)
   );
 header('Content-type:application/json;charset=utf-8');
 echo json_encode($response);
